55b123b86a5620f6bd8f300cd0b34898e8b28419
AzarkevichO <azarkevich.o@yandex.ru>
2016-11-04 15:30:54 +0300

    SpreadingCoefficient

---
+++ EGH01/EGH01/Models/EGHRGE/SreadingCoefficientView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EGH01.Models.EGHRGE
+{
+    public class SreadingCoefficientView
+    {
+        //
+        // GET: /SreadingCoefficientView/
+        
+        public EGH01DB.Types.GroundType groundtype {get; set;}
+        public float volume {get; set;}
+        public float angle {get; set;}    
+        
+	}
+}
