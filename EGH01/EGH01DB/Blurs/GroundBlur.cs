69b98df8c07c65570793ec00ccc41cf63e16b7a3
smw60 <smw60@mail.ru>
2016-11-11 03:20:16 +0300

    forecast

--- EGH01/EGH01DB/Blurs/GroundBlur.cs
+++ EGH01/EGH01DB/Blurs/GroundBlur.cs
@@ -22,10 +22,13 @@ namespace EGH01DB.Blurs
         public float                square               {get; private set;}       // площадь (м2)     
         public float                radius               {get; private set;}       // радиус наземного пятна (м)     
         public float                totalmass            {get; private set;}       // масса пролива (кг)    
-        public float                limitadsorbedmass    {get; private set;}       // максиальная маса нефтепродукта, кот. может быть адсорбирована грунтом (кг) 
+        public float                limitadsorbedmass    {get; private set;}       // максиальная маса нефтепродукта, кот. может быть адсорбирована грунтом (т) 
         public float                avgheight            {get; private set;}       // средняя глубина грунтовых вод по опорным точкам (м) 
-       // public float                h    
-       
+        public float                adsorbedmass         {get; private set;}       // адсобированная масса нефтепрдукта грунтом (т)  
+        public float                restmass             {get; private set;}       //  масса нефтепродукта достигшая грунтовых вод  (т)  
+        public float                depth                {get; private set;}       //  глубина проникновения нефтепродукта в грунт (м)  
+
+
         public  AnchorPointList     anchorpointlist      {get; private set;}       // список опорных точек, попаших в наземное пятно загрязнения    
 
         public WaterProperties waterproperties           { get; private set; }       // физико-химические свойства воды  
@@ -82,8 +85,15 @@ namespace EGH01DB.Blurs
                                      this.spreadpoint.petrochemicaltype.tension                              *  // коэфициент поверхностного натяжения нефтепрдукта 
                                      (float)Math.Pow(this.waterproperties.viscocity, 2)                         //  вязкость воды  
                                      );
-
             }
+            
+            this.adsorbedmass = (limitadsorbedmass >= this.totalmass ? this.totalmass : limitadsorbedmass);      // адсорбированная масса нефтепродукта в грунте  
+
+            this.restmass =    (this.adsorbedmass >= this.totalmass ? 0 : this.totalmass - this.adsorbedmass);  // масса нефтепродукта достигшая грунтовых вод 
+
+             this.depth =      (this.restmass > 0 ? this.avgheight : this.avgheight * (this.totalmass / this.limitadsorbedmass)); // глубина проникновения нефтепродукта в грунт     
+
+           
 
             this.ecoobjecstlist = EcoObjectsList.CreateEcoObjectsList(spreadpoint, radius);
             this.pollutionlist = GroundPollutionList.CreateGroundPollutionList(spreadpoint, radius);
