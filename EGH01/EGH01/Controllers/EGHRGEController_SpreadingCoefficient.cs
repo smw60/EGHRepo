355fe7f4c79fbc6ec377c0a63a218645d44c30e1
AzarkevichO <azarkevich.o@yandex.ru>
2016-11-14 15:11:23 +0300

    правки

--- EGH01/EGH01/Controllers/EGHRGEController_SpreadingCoefficient.cs
+++ EGH01/EGH01/Controllers/EGHRGEController_SpreadingCoefficient.cs
@@ -16,7 +16,7 @@ namespace EGH01.Controllers
     public partial class EGHRGEController : Controller
     {
 
-        public ActionResult SpreadingCoefficient()
+        public ActionResult SpreadingCoefficient(SpreadingCoefficientView scv)
         {
 
             RGEContext db = null;
@@ -36,64 +36,79 @@ namespace EGH01.Controllers
                     view = View("SpreadingCoefficientCreate");
 
                 }
+
                 else if (menuitem.Equals("SpreadingCoefficient.Delete"))
                 {
-                    SpreadingCoefficient sc = new SpreadingCoefficient();
-                    //if (EGH01DB.Primitives.SpreadingCoefficient.GetByData(db, c, ref pt))
-                    //{
-                        view = View("SpreadingCoefficientDelete", sc);
+                    EGH01DB.Types.GroundType type_grounde = new EGH01DB.Types.GroundType();
+                    if (EGH01DB.Types.GroundType.GetByCode(db, scv.list_groundType, out type_grounde))
+                    {
+                        //GroundType ground_type = new GroundType(scv.list_groundType, type_groud.name, type_groud.porosity, type_groud.holdmigration, type_groud.waterfilter, type_groud.diffusion,
+                        //type_groud.distribution, type_groud.sorption, type_groud.watercapacity, type_groud.soilmoisture, type_groud.аveryanovfactor, type_groud.permeability);
+
+                        string strmin_angle = this.HttpContext.Request.Params["min_angle"] ?? "Empty";
+                        float min_angle;
+                        Helper.FloatTryParse(strmin_angle, out min_angle);
+
+                        string strmax_angle = this.HttpContext.Request.Params["max_angle"] ?? "Empty";
+                        float max_angle;
+                        Helper.FloatTryParse(strmax_angle, out max_angle);
+
+                        string strmin_volume = this.HttpContext.Request.Params["min_volume"] ?? "Empty";
+                        float min_volume;
+                        Helper.FloatTryParse(strmin_volume, out min_volume);
+
+                        string strmax_volume = this.HttpContext.Request.Params["max_volume "] ?? "Empty";
+                        float max_volume;
+                        Helper.FloatTryParse(strmax_volume, out max_volume);
+
+                        string strkoef = this.HttpContext.Request.Params["koef"] ?? "Empty";
+                        float koef;
+                        Helper.FloatTryParse(strkoef, out koef);
+
+
+                        float volume = max_volume - min_volume;
+                        float angle = max_angle - min_angle;
+                        SpreadingCoefficient sc = new EGH01DB.Primitives.SpreadingCoefficient((GroundType)type_grounde, (float)min_volume, (float)max_volume, (float)min_angle, (float)max_angle, (float)koef);
+
                     
-                }
-                //else if (menuitem.Equals("SpreadingCoefficient.Delete"))
-                //{
-                //    string type_code_item = this.HttpContext.Request.Params["type_code"];
-                //    if (type_code_item != null)
-                //    {
-                //        int c = 0;
-                //        if (int.TryParse(type_code_item, out c))
-                //        {
-                //            SpreadingCoefficient pt = new SpreadingCoefficient();
-                //            if (EGH01DB.Primitives.SpreadingCoefficient.GetByData(db, c, ref pt))
-                //            {
-                //                view = View("SpreadingCoefficientDelete", pt);
-                //            }
-                //        }
-                //    }
-                //}
+                        //if (EGH01DB.Primitives.SpreadingCoefficient.GetByData(db, (GroundType)type_grounde,(float)volume,(float)angle))
+                        //{
+                            view = View("SpreadingCoefficientDelete", sc);
+                        //}
+
+                    }
+
+                    //else if (menuitem.Equals("SpreadingCoefficient.Update"))
+                    //{
+                    //    string type_code_item = this.HttpContext.Request.Params["type_code"];
+
+                    //    if (type_code_item != null)
+                    //    {
+                    //        int c = 0;
+                    //        if (int.TryParse(type_code_item, out c))
+                    //        {
+                    //            EGH01DB.Primitives.SpreadingCoefficient sc = new EGH01DB.Primitives.SpreadingCoefficient();
+                    //            if (EGH01DB.Primitives.SpreadingCoefficient.GetByData()
+                    //            {
+                    //                view = View("SpreadingCoefficientUpdate", pt);
+                    //            }
+                    //        }
+                    //    }
+                    //}
+                    else if (menuitem.Equals("SpreadingCoefficient.Excel"))
+                    {
+                        //EGH01DB.Types.PetrochemicalType.PetrochemicalTypeList list = new EGH01DB.Types.PetrochemicalType.PetrochemicalTypeList();
+                        //XmlNode node = list.toXmlNode();
+                        //XmlDocument doc = new XmlDocument();
+                        //XmlNode nnode = doc.ImportNode(node, true);
+                        //doc.AppendChild(nnode);
+                        //doc.Save(Server.MapPath("~/App_Data/PetrochemicalType.xml"));
+                        //view = View("Index");
+
+                        //view = File(Server.MapPath("~/App_Data/PetrochemicalType.xml"), "text/plain", "PetrochemicalType.xml");
 
 
-
-
-                //else if (menuitem.Equals("SpreadingCoefficient.Update"))
-                //{
-                //    string type_code_item = this.HttpContext.Request.Params["type_code"];
-
-                //    if (type_code_item != null)
-                //    {
-                //        int c = 0;
-                //        if (int.TryParse(type_code_item, out c))
-                //        {
-                //            EGH01DB.Primitives.SpreadingCoefficient pt = new EGH01DB.Primitives.SpreadingCoefficient();
-                //            if (EGH01DB.Primitives.SpreadingCoefficient.GetByData(db, c, ref pt))
-                //            {
-                //                view = View("SpreadingCoefficientUpdate", pt);
-                //            }
-                //        }
-                //    }
-                //}
-                else if (menuitem.Equals("SpreadingCoefficient.Excel"))
-                {
-                    //EGH01DB.Types.PetrochemicalType.PetrochemicalTypeList list = new EGH01DB.Types.PetrochemicalType.PetrochemicalTypeList();
-                    //XmlNode node = list.toXmlNode();
-                    //XmlDocument doc = new XmlDocument();
-                    //XmlNode nnode = doc.ImportNode(node, true);
-                    //doc.AppendChild(nnode);
-                    //doc.Save(Server.MapPath("~/App_Data/PetrochemicalType.xml"));
-                    //view = View("Index");
-
-                    //view = File(Server.MapPath("~/App_Data/PetrochemicalType.xml"), "text/plain", "PetrochemicalType.xml");
-
-
+                    }
                 }
             }
             catch (RGEContext.Exception e)
@@ -129,9 +144,8 @@ namespace EGH01.Controllers
                     EGH01DB.Types.GroundType type_groud = new EGH01DB.Types.GroundType();
                     if (EGH01DB.Types.GroundType.GetByCode(db, scv.list_groundType, out type_groud))
                     {
-                        GroundType ground_type = new GroundType(scv.list_groundType, type_groud.name, type_groud.porosity, type_groud.holdmigration, type_groud.waterfilter, type_groud.diffusion,
-                        type_groud.distribution, type_groud.sorption, type_groud.watercapacity, type_groud.soilmoisture, type_groud.аveryanovfactor, type_groud.permeability);
-
+                        //GroundType ground_type = new GroundType(scv.list_groundType, type_groud.name, type_groud.porosity, type_groud.holdmigration, type_groud.waterfilter, type_groud.diffusion,
+                        //type_groud.distribution, type_groud.sorption, type_groud.watercapacity, type_groud.soilmoisture, type_groud.аveryanovfactor, type_groud.permeability);
 
                         string strmin_angle = this.HttpContext.Request.Params["min_angle"] ?? "Empty";
                         float min_angle;
@@ -145,7 +159,7 @@ namespace EGH01.Controllers
                         float min_volume;
                         Helper.FloatTryParse(strmin_volume, out min_volume);
 
-                        string strmax_volume = this.HttpContext.Request.Params["max_volume "] ?? "Empty";
+                        string strmax_volume = this.HttpContext.Request.Params["max_volume"] ?? "Empty";
                         float max_volume;
                         Helper.FloatTryParse(strmax_volume, out max_volume);
 
@@ -153,7 +167,7 @@ namespace EGH01.Controllers
                         float koef;
                         Helper.FloatTryParse(strkoef, out koef);
 
-                        SpreadingCoefficient sc = new EGH01DB.Primitives.SpreadingCoefficient(ground_type, (float)min_volume, (float)max_volume, (float)min_angle, (float)max_angle, (float)koef);
+                        SpreadingCoefficient sc = new EGH01DB.Primitives.SpreadingCoefficient(type_groud, (float)min_volume, (float)max_volume, (float)min_angle, (float)max_angle, (float)koef);
 
                         koef = EGH01DB.Primitives.SpreadingCoefficient.Get(db, sc);
 
@@ -179,6 +193,37 @@ namespace EGH01.Controllers
             return view;
         }
 
+        [HttpPost]
+        public ActionResult SpreadingCoefficientDelete(SpreadingCoefficient sc)
+        {
+            RGEContext db = null;
+            ViewBag.EGHLayout = "CCO";
+            ActionResult view = View("Index");
+            string menuitem = this.HttpContext.Request.Params["menuitem"] ?? "Empty";
+            try
+            {
+                db = new RGEContext();
+                if (menuitem.Equals("PetrochemicalType.Delete.Delete"))
+                {
+                    //SpreadingCoefficient sc = new EGH01DB.Primitives.SpreadingCoefficient();
+                    if (EGH01DB.Primitives.SpreadingCoefficient.Delete(db, sc))
+                        view = View("SpreadingCoefficient", db);
+                }
+                else if (menuitem.Equals("SpreadingCoefficient.Delete.Cancel"))
+                    view = View("SpreadingCoefficient", db);
+
+            }
+            catch (RGEContext.Exception e)
+            {
+                ViewBag.msg = e.message;
+            }
+            catch (Exception e)
+            {
+                ViewBag.msg = e.Message;
+            }
+
+            return view;
+        }
 
     }
 }
