ee24d74222d8429eba894e072ac208832cfaa3a7
smw60 <smw60@mail.ru>
2016-11-18 13:45:05 +0300

    map

---
+++ EGH01/EGH01/Controllers/EGHCAIControllerMap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using EGH01.Models.EGHCAI;
+using EGH01DB;
+using EGH01DB.Points;
+using EGH01DB.Primitives;
+using EGH01DB.Types;
+
+
+namespace EGH01.Controllers
+{
+    public partial  class EGHCAIController:Controller
+    {
+
+        public ActionResult Map()
+        {
+
+
+
+            return View();
+         }
+
+
+
+    }
+}
