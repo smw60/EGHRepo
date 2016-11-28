b261038453e63bf751c2a37c86c8b91ffd098939
AzarkevichO <azarkevich.o@yandex.ru>
2016-11-28 10:52:51 +0300

    Классификация аварий

---
+++ EGH01/EGH01/Models/EGHORT/EmergencyClassView.cs
@@ -0,0 +1,10 @@
+﻿namespace EGH01.Models.EGHORT
+{
+    public class EmergencyClassView            // Классификация аварий
+    {
+        public int    type_code { get; set; }   // код категории
+        public string name      { get; set; }   // наименование категории
+        public float  minmass   { get; set; }   // минимальное значение диапазона
+        public float  maxmass   { get; set; }   // максимальное значение диапазона
+    }
+}
