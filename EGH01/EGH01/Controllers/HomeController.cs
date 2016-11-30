aa839db3a90819f94385611a4588d24692c62cce
DaryaCherniak <darya_cherniak@inbox.ru>
2016-11-30 15:46:36 +0300

    Context

--- EGH01/EGH01/Controllers/HomeController.cs
+++ EGH01/EGH01/Controllers/HomeController.cs
@@ -26,29 +26,6 @@ namespace EGH01.Controllers
 
             return View();
         }
-<<<<<<< HEAD
-        public ActionResult Map()
-        {
-            ViewBag.Message = "Map";
 
-            return View();
-        }
-        public ActionResult Upload(HttpPostedFileBase upload)
-        {
-            ViewBag.Message = "Map";
-            ActionResult view = View("Index");
-            view = View("Map");
-            if (upload != null)
-            {
-             
-                // сохраняем файл в папку Files в проекте
-                upload.SaveAs(Server.MapPath("~/App_Data/RiskObject.xml"));
-            }
-      
-            return view;
-        }
-=======
-
->>>>>>> b6cbc93d3ad47ba444d2f02efcf9a4f6718f3684
     }
 }
